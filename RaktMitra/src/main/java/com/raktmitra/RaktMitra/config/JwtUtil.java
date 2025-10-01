9fa02fcd22870aa6258a0224b79a38bf1491c5ea
ayushchoyal <ayushchoyal07@gmail.com>
2025-10-01 10:41:01 +0530

    Add files via upload

---
+++ RaktMitra/src/main/java/com/raktmitra/RaktMitra/config/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.raktmitra.RaktMitra.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    // Option 1: Fixed secret key (≥ 32 bytes)
+    private static final String SECRET_STRING = "myverysecureandlongsecretkeywith32bytes!";
+    private final Key key = Keys.hmacShaKeyFor(SECRET_STRING.getBytes(StandardCharsets.UTF_8));
+
+    // Generate token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 ))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Extract username
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // Validate token
+    public boolean validateToken(String token, String username) {
+        String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    // Check expiration
+    private boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expiration.before(new Date());
+    }
+}
---
+++ RaktMitra/src/main/java/com/raktmitra/RaktMitra/repository/DonorRepository.java
@@ -0,0 +1,7 @@
+package com.raktmitra.RaktMitra.repository;
+
+import com.raktmitra.RaktMitra.entity.Donar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonorRepository extends JpaRepository<Donar,Long> {
+}
